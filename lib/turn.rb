8e034534260b071603417df4a793d05939d6aa93
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-13 23:38:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,9 +37,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
       index = move(board, index, marker = "X")
-display_board
-    else
-      turn(board)
+
 
 
 end
